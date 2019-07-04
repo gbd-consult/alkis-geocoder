d85c2a55abd2a69bf5fe9f3a8cb9362c5b89be64
towa <7078986+towa@users.noreply.github.com>
2019-07-04 14:00:34 +0200

    bugfix for windows

--- alkis_geocoder/alkis_geocoder.py
+++ alkis_geocoder/alkis_geocoder.py
@@ -48,7 +48,7 @@ class AlkisGeocoder:
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
+        locale = str(QSettings().value('locale/userLocale'))[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
